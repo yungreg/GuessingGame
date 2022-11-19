85a317ac84f7f409658ec47820acaeeba3c6d300
Reginald Williams Jr <rlwjr433@gmail.com>
2022-11-18 19:16:27 -0600

    75% done with Phase 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+*todo: Display a message to the user asking them to guess the secret number.
+*todo: Display a prompt for the user's guess.
+*todo: Take the user's guess as input and save it as a variable.
+todo: Display the user's guess back to the screen.
+*/
+
+using System;
 
 namespace GuessingGame
 {
@@ -6,7 +13,13 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello user! Guess that secret number, n3rd.");
+            Console.Write("What's your guess? input it here:");
+            Console.ReadLine();
+            int UserAnswer = Console.ReadLine();
+
+            Console.Write(UserAnswer);
         }
     }
 }
+
